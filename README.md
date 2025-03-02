b0241415e4741e0b340c3789e38f3ebb6b1e6ec7
Norte <nortenavi@gmail.com>
2025-03-02 23:58:38 +0200

    Added function for the largest file & small fixes

--- README.md
+++ README.md
@@ -1,14 +1,15 @@
 # demouser-acc-files
 
-This template should help get you started developing with Vue 3 in Vite.
+Demo application for
 
-## Recommended IDE Setup
+## User credentials
 
-[VSCode](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar) (and disable Vetur).
+Create .env.local file and add:
 
-## Customize configuration
-
-See [Vite Configuration Reference](https://vite.dev/config/).
+```
+VITE_API_USERNAME=user
+VITE_API_PASSWORD=password
+```
 
 ## Project Setup
 
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -9,14 +9,17 @@
     } from "@heroicons/vue/24/solid";
 
     const data = ref([]);
-    const error = ref(null);
     const currentPath = ref("");
     const pathHistory = ref([]);
+    const isLoading = ref(false);
+    const apiUrl = "/filemanager/list?path=";
 
     const fetchData = async (path = "") => {
+        largestFile.value = null;
+        isLoading.value = true;
         try {
             const response = await api.get(
-                `/filemanager/list?path=${encodeURIComponent(path)}`
+                `${apiUrl}${encodeURIComponent(path)}`
             );
 
             const dirs = response.data.files.filter(
@@ -30,7 +33,9 @@
 
             currentPath.value = path;
         } catch (err) {
-            error.value = err.message;
+            console.log("Error occurred");
+        } finally {
+            isLoading.value = false;
         }
     };
 
@@ -53,9 +58,67 @@
         }
     };
 
-    const biggestFile = ref(null);
+    const largestFile = ref(null);
 
-    const getBiggestFile = async () => {};
+    const fetchWithRetry = async (url, retries = 3, delay = 3000) => {
+        let attempt = 0;
+        while (attempt < retries) {
+            try {
+                const response = await api.get(url);
+                return response.data;
+            } catch (error) {
+                attempt++;
+
+                if (attempt >= retries) {
+                    throw error;
+                }
+
+                await new Promise((resolve) => setTimeout(resolve, delay));
+            }
+        }
+    };
+    const getLargestFile = async () => {
+        isLoading.value = true;
+        largestFile.value = null;
+        pathHistory.value = [];
+        currentPath.value = "";
+        const queue = [""];
+        let largest = { name: "", sizeBytes: 0 };
+
+        while (queue.length) {
+            const currentDir = queue.shift();
+            try {
+                const data = await fetchWithRetry(
+                    `${apiUrl}${encodeURIComponent(currentDir)}`
+                );
+
+                if (!data || !data.files) {
+                    continue;
+                }
+
+                data.files.forEach((file) => {
+                    if (
+                        file.type === "file" &&
+                        file.sizeBytes > largest.sizeBytes
+                    ) {
+                        largest = file;
+                    }
+                    if (file.type === "dir") {
+                        queue.push(
+                            currentDir
+                                ? `${currentDir}/${file.name}`
+                                : file.name
+                        );
+                    }
+                });
+            } catch (error) {
+                console.log("Error occurred");
+            }
+        }
+
+        largestFile.value = largest;
+        isLoading.value = false;
+    };
 
     onMounted(fetchData);
 </script>
@@ -66,7 +129,7 @@
 
         <div class="relative mb-4 flex items-center w-full">
             <button
-                v-if="pathHistory.length"
+                v-if="pathHistory.length && currentPath !== ''"
                 @click="goBack"
                 type="button"
                 class="text-white dark:bg-gray-700 dark:border-gray-600 dark:hover:bg-gray-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center cursor-pointer"
@@ -78,21 +141,33 @@
             <div
                 class="absolute left-1/2 transform -translate-x-1/2 text-center flex items-center"
             >
+                <button
+                    @click="fetchData('')"
+                    :disabled="isLoading"
+                    class="text-gray-700 dark:text-gray-300 text-md font-bold hover:underline focus:outline-none cursor-pointer disabled:cursor-not-allowed disabled:text-gray-400"
+                >
+                    root/
+                </button>
                 <span
                     class="text-gray-700 dark:text-gray-300 text-md font-bold"
                 >
-                    Root: {{ currentPath || "/" }}
+                    {{
+                        currentPath && currentPath !== ""
+                            ? currentPath.split("/").join("/")
+                            : ""
+                    }}
                 </span>
             </div>
 
             <div class="ml-auto">
                 <button
-                    @click="getBiggestFile"
+                    @click="getLargestFile"
                     type="button"
-                    class="text-white dark:bg-gray-700 dark:border-gray-600 dark:hover:bg-gray-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center cursor-pointer"
+                    class="text-white dark:bg-gray-700 dark:border-gray-600 dark:hover:bg-gray-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center cursor-pointer disabled:cursor-not-allowed disabled:text-gray-400"
+                    :disabled="isLoading"
                 >
                     <MagnifyingGlassIcon class="size-6 text-white mr-1.5" />
-                    Find biggest file
+                    Largest file
                 </button>
             </div>
         </div>
@@ -114,39 +189,74 @@
                         </tr>
                     </thead>
                     <tbody>
-                        <tr
-                            v-if="data.length"
-                            v-for="item in data"
-                            :key="item.name"
-                            class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200 hover:bg-gray-50 dark:hover:bg-gray-600"
-                            :class="{ 'cursor-pointer': item.type === 'dir' }"
-                            @click="openDirectory(item)"
-                        >
+                        <tr v-if="isLoading">
                             <td
-                                class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                                colspan="3"
+                                class="p-4 text-center font-bold uppercase dark:text-gray-400"
                             >
-                                <component
-                                    :is="
-                                        item.type === 'dir'
-                                            ? FolderIcon
-                                            : DocumentTextIcon
-                                    "
-                                    class="text-white size-6"
-                                />
-                            </td>
-                            <td class="px-6 py-4">{{ item.name }}</td>
-                            <td class="px-6 py-4">{{ item.sizeBytes }}</td>
-                        </tr>
-                        <tr
-                            v-else
-                            class="bg-white dark:bg-gray-800 dark:border-gray-700 border-gray-200"
-                        >
-                            <td colspan="3">
-                                <div class="p-4 uppercase">
-                                    This folder is empty
+                                <div
+                                    class="flex justify-center items-center gap-2 relative"
+                                >
+                                    <span class="relative z-10">
+                                        Fetching data
+                                    </span>
+                                    <div
+                                        class="loader w-3 h-3 -ml-2 relative top-[2px]"
+                                    ></div>
                                 </div>
                             </td>
                         </tr>
+
+                        <tr
+                            v-else-if="largestFile"
+                            class="bg-white dark:bg-gray-800 dark:border-gray-700 border-gray-200 hover:bg-gray-50 dark:hover:bg-gray-600"
+                        >
+                            <td class="px-6 py-4">
+                                <DocumentTextIcon class="text-white size-6" />
+                            </td>
+                            <td class="px-6 py-4">{{ largestFile.name }}</td>
+                            <td class="px-6 py-4">
+                                {{ largestFile.sizeBytes }}
+                            </td>
+                        </tr>
+
+                        <template v-else>
+                            <tr
+                                v-if="data.length"
+                                v-for="item in data"
+                                :key="item.name"
+                                class="bg-white dark:bg-gray-800 dark:border-gray-700 border-gray-200 hover:bg-gray-50 dark:hover:bg-gray-600"
+                                :class="{
+                                    'cursor-pointer': item.type === 'dir',
+                                }"
+                                @click="openDirectory(item)"
+                            >
+                                <td
+                                    class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                                >
+                                    <component
+                                        :is="
+                                            item.type === 'dir'
+                                                ? FolderIcon
+                                                : DocumentTextIcon
+                                        "
+                                        class="text-white size-6"
+                                    />
+                                </td>
+                                <td class="px-6 py-4">{{ item.name }}</td>
+                                <td class="px-6 py-4">{{ item.sizeBytes }}</td>
+                            </tr>
+                            <tr
+                                v-else
+                                class="bg-white dark:bg-gray-800 dark:border-gray-700 border-gray-200"
+                            >
+                                <td colspan="3">
+                                    <div class="p-4 uppercase">
+                                        This folder is empty
+                                    </div>
+                                </td>
+                            </tr>
+                        </template>
                     </tbody>
                 </table>
             </div>
